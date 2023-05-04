004ed1e5048791fbf3094dfb616a5eb20732f6ef
aysel <aysel.ahmadli@gmail.com>
2023-05-04 10:13:14 +0300

    wise quarter

---
+++ src/day24_inheritance/EToyota.java
@@ -0,0 +1,11 @@
+package src.day24_inheritance;
+
+public class EToyota extends DAraba {
+
+    EToyota(){
+        System.out.println("Toyota constructor calisti");
+    }
+    String uretimYeri;
+    String marka= "Toyota";
+}
+
