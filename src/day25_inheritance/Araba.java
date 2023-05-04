aa189e471bcf2cd9622d20021cf7c21d9a788fd2
aysel <aysel.ahmadli@gmail.com>
2023-05-04 22:27:35 +0300

    wise quarter

---
+++ src/day25_inheritance/Araba.java
@@ -0,0 +1,13 @@
+package src.day25_inheritance;
+
+public class Araba {
+
+    Araba(){
+        System.out.println("Araba constructor calisti");
+    }
+    String model="Arabalarin modeli olur";
+    String motor= "Arabalarda motor olur";
+    String marka="Arabalarda marka olur";
+}
+
+
