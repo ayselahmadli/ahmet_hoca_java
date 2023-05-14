ade719bc0e5bcc32c98c58a2d04cd5bceb73164e
aysel <aysel.ahmadli@gmail.com>
2023-05-15 00:35:26 +0300

    wise quarter

---
+++ src/day26_inheritanceDataTypeKullanimi_overriding/BKuslar.java
@@ -0,0 +1,10 @@
+package src.day26_inheritanceDataTypeKullanimi_overriding;
+
+public class BKuslar extends AHayvan {
+
+    String kanat= "kanatlidirlar";
+    String solunum= "akcigerle nefes alirlar";
+    String gaga= "gagalari vardir";
+    String cogalma= "yumurtayla cogalirlar";
+}
+
